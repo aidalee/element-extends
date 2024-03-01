e3f3ea5622f4a96b6413d705ff44de895d8b66db
please <please@xiaotata.local>
2024-03-01 17:14:46 +0800

    fix

--- packages/so-ui/components/button/index.ts
+++ packages/so-ui/components/button/index.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: please
+ * @Date: 2024-02-29 13:44:18
+ * @LastEditors: please
+ * @LastEditTime: 2024-03-01 17:10:56
+ * @Description: 请填写简介
+ */
 import type { App } from 'vue'
 import Button from './button'
 
